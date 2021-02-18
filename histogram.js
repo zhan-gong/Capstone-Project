3a3b4f7dab048afbd53c98e7f9de0c9d5472e8f0
zhan-gong <60577804+zhan-gong@users.noreply.github.com>
2021-02-18 12:55:57 -0500

    1

---
+++ histogram.js
@@ -0,0 +1,71 @@
+
+d3.csv("../data/Dota2_hearoes_data.csv", d3.autoType).then(data => {
+  console.log(data);
+
+  const width = window.innerWidth * 0.9,
+    height = 2500,
+    paddingInner = 0.2,
+    margin = { top: 20, bottom: 40, left: 100, right: 100 };
+  
+  const barColor = d3
+    .scaleLinear()
+    .domain([d3.min(data, d => d.Count), d3.max(data, d => d.Count)])
+    .range([d3.interpolateGreens(0.5), d3.interpolateGreens(1)]);
+
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map(d => d.Name))
+    .range([margin.top, height - margin.bottom])
+    .paddingInner(paddingInner);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([d3.min(data, d => d.Count), d3.max(data, d => d.Count)])
+    .range([0, width - 3*margin.right]);
+
+  const yAxis = d3.axisLeft(yScale)
+
+  const svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const rect = svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("y", d => yScale(d.Name) + margin.top)
+    .attr("x", d => xScale(d3.min(data, d => d.Count))+margin.left)
+    .attr("width", d => xScale(d.Count))
+    .attr("height", yScale.bandwidth())
+    .attr("fill", d => barColor(d.Count))
+
+  const text = svg
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .attr("y", d => yScale(d.Name) + margin.top - 10)
+    .attr("x", d => xScale(d.Count) + 110)
+    .text(d => d.Count)
+    .attr("fill", "green")
+    .attr("dy", "1.5em");
+
+  svg
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate("+ margin.left +", "+ margin.top +")")
+    .call(yAxis);
+});
+  // svg
+  //   .append("circle")
+  //   .attr("class", "logo")
+  //   .attr("cx", 700)
+  //   .attr("cy", 300)
+  //   .attr("r", 10)
+  //   .attr("fill", "white")
+  //   .attr("stroke", "white") 
+  //   .attr("stroke-width", 0.25)
+  //   .attr("class", "axis")
+  //   .attr("transform", "translate("+ margin.left +", "+ margin.top +")")
+  //   });
---
+++ main.js
@@ -0,0 +1,52 @@
+
+d3.csv("../data/Dota2_hearoes_data.csv", d3.autoType).then(data => {
+  console.log(data);
+
+  const width = window.innerWidth * 0.9,
+    height = window.innerHeight / 3,
+    paddingInner = 0.2,
+    margin = { top: 20, bottom: 40, left: 40, right: 40 };
+
+  const barColor = d3
+    .scaleLinear()
+    .domain([d3.min(data, d => d.Count), d3.max(data, d => d.Count)])
+    .range([d3.interpolateGreens(0.5), d3.interpolateGreens(1)]);
+
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map(d => d.Name))
+    .range([margin.left, width - margin.right])
+    .paddingInner(paddingInner);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, d => d.Count)])
+    .range([height - margin.bottom, margin.top]);
+
+  const svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const rect = svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("y", d => yScale(d.Count))
+    .attr("x", d => xScale(d.City))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - margin.bottom - yScale(d.Count))
+    .attr("fill", d => barColor(d.Count));
+
+  const text = svg
+    .selectAll("text")
+    .data(data)
+    .enter().append("text")
+    .text(d => d.Count)
+      .attr("x", d => xScale(d.City) + (xScale.bandwidth() / 2) - 14)
+      .attr("y", d => yScale(d.Count))
+      .attr("dy", "-0.5em");
+
+
+});
